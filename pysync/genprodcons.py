89323942510cb3808dd75b1747f7744fddfd5430
pipesama500 <felipemartinezc004@gmail.com>
2025-05-20 06:20:07 -0500

    GenProdcons  commit

---
+++ pysync/genprodcons.py
@@ -0,0 +1,23 @@
+from threading import Condition
+from collections import deque
+
+class GenProdCons:
+    def __init__(self, size):
+        self.buffer = deque(maxlen=size)
+        self.size = size
+        self.condition = Condition()
+
+    def put(self, e):
+        with self.condition:
+            while len(self.buffer) == self.size:
+                self.condition.wait()
+            self.buffer.append(e)
+            self.condition.notify_all()
+
+    def get(self):
+        with self.condition:
+            while len(self.buffer) == 0:
+                self.condition.wait()
+            e = self.buffer.popleft()
+            self.condition.notify_all()
+            return e
