d9cb95f7e5556a1733a4e1b80775351d8c21be21
Sebastian Jimenez <anderjime864@gmail.com>
2025-05-21 11:33:38 -0500

    update tests

--- test/prod_cons_test_sync.py
+++ test/prod_cons_test_sync.py
@@ -1,5 +1,8 @@
+# Test Basic
+
 import os
 import unittest
+from queue import Queue
 from threading import Thread
 import importlib
 
@@ -13,28 +16,161 @@ else:
 
 prod_cons_imprt = importlib.__import__(prod_cons_mdl, globals(), locals(), [], 0)
 
-def basic_producer(prod_cons, times, output):
-    for i in range(times):
+def basic_producer(prod_cons, q, times=100):
+    prod_values = []
+    for i in range(0,times):
         prod_cons.put(i)
-        output.append(i)
+        prod_values.append(i)
 
-def basic_consumer(prod_cons, times, output):
-    for _ in range(times):
-        value = prod_cons.get()
-        output.append(value)
+    q.put(prod_values)
+        
+def basic_consumer(prod_cons, q, times=100):
+    cons_values = []
+    for i in range(0,times):
+        cons_values.append(prod_cons.get())
+        
+    q.put(cons_values)
+
+def generic_producer(prod_cons, q, times=100):
+    values = [ 'Hola', 1.2, True, False, 2, (True, False), { 'uno' : 1 } ]
+    prod_values = []
+    idx = 0
+    for i in range(0,times):
+        prod_cons.put(values[idx])
+        prod_values.append(values[idx])
+        idx = (idx + 1) % len(values)
+
+    q.put(prod_values)
 
 class TestProdConsTestSync(unittest.TestCase):
+
     def test_prod_cons_all(self):
         prod_cons = prod_cons_imprt.GenProdCons()
-        produced = []
-        consumed = []
-
-        prod_thr = Thread(target=basic_producer, args=(prod_cons, 100, produced))
-        cons_thr = Thread(target=basic_consumer, args=(prod_cons, 100, consumed))
-
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=basic_producer,args=(prod_cons, prod_q, 100))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 100))
         prod_thr.start()
         cons_thr.start()
         prod_thr.join()
         cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
 
-        self.assertEqual(produced, consumed) 
+    def test_prod_cons_size_one_hundred(self):
+        prod_cons = prod_cons_imprt.GenProdCons(100)
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=basic_producer,args=(prod_cons, prod_q, 100))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 100))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_cons_size_50(self):
+        prod_cons = prod_cons_imprt.GenProdCons()
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=basic_producer,args=(prod_cons, prod_q, 50))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 50))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_cons_size_50(self):
+        prod_cons = prod_cons_imprt.GenProdCons(1)
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=basic_producer,args=(prod_cons, prod_q, 50))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 50))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_cons_two(self):
+        prod_cons = prod_cons_imprt.GenProdCons(1)
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=basic_producer,args=(prod_cons, prod_q, 2))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 2))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_cons_generic_producer_100(self):
+        prod_cons = prod_cons_imprt.GenProdCons()
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=generic_producer,args=(prod_cons, prod_q, 100))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 100))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_cons_generic_producer_100(self):
+        prod_cons = prod_cons_imprt.GenProdCons()
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=generic_producer,args=(prod_cons, prod_q, 100))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 100))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_cons_generic_producer_one(self):
+        prod_cons = prod_cons_imprt.GenProdCons(1)
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=generic_producer,args=(prod_cons, prod_q, 100))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 100))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join()
+        cons_thr.join()
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(prod_values,cons_values)
+
+    def test_prod_100_cons_50(self):
+        prod_cons = prod_cons_imprt.GenProdCons(50)
+        prod_q = Queue()
+        cons_q = Queue()
+        prod_thr = Thread(target=basic_producer,args=(prod_cons, prod_q, 100))
+        cons_thr = Thread(target=basic_consumer,args=(prod_cons, cons_q, 50))
+        prod_thr.start()
+        cons_thr.start()
+        prod_thr.join(10)
+        cons_thr.join(10)
+        if prod_thr.is_alive():
+            self.fail(msg="Producer fail")
+        if cons_thr.is_alive():
+            self.fail(msg="Consumer fail")
+        prod_values = prod_q.get()
+        cons_values = cons_q.get()
+        self.assertEqual(len(prod_values),100)
+        self.assertEqual(len(cons_values), 50)
